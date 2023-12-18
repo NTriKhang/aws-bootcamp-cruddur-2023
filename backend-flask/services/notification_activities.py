d4623b3fabf73d8464883b7d97424bfdc574d5ab
N.TriKhang Student <nguyentrikhang1703@gmail.com>
2023-12-18 02:52:54 +0000

    enable notifications page

---
+++ backend-flask/services/notification_activities.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta, timezone
+class NotificationActivities:
+  def run():
+    now = datetime.now(timezone.utc).astimezone()
+    results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'Andrew Brown',
+      'message': 'Cloud is not fun!',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 5,
+      'replies_count': 1,
+      'reposts_count': 0,
+      'replies': [],
+    },
+    {
+      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+      'handle':  'Worf',
+      'message': 'I am out of prune juice',
+      'created_at': (now - timedelta(days=7)).isoformat(),
+      'expires_at': (now + timedelta(days=9)).isoformat(),
+      'likes': 0,
+      'replies': []
+    },
+    ]
+    return results
